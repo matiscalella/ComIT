d8876d9ae164ee126615e6e0ab91d542c3858b6b
Matias Scalella <matias.scalella@live.com>
2017-08-28 21:33:02 -0300

    mas ejercicios

---
+++ JavaScript_M.Scalella/ej117.js
@@ -0,0 +1,11 @@
+let alumnos = [
+    'Matias',
+    'Nicolas',
+    'Cristian',
+    'Jonas',
+    'Alejandro'
+];
+
+console.log(alumnos[0]);
+console.log(alumnos[3]);
+console.log(alumnos[4]);
---
+++ JavaScript_M.Scalella/ej118.js
@@ -0,0 +1,16 @@
+let notas = [
+    6,
+    5,
+    7,
+    9.5,
+    5,
+    6,
+    7.5,
+    8,
+    10,
+    10
+];
+
+let resultado = (notas[0] + notas[3] + notas[4] + notas[9]);
+
+console.log(resultado);
